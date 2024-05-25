1da99c51e4056df41eaaa413f7c2f6adf35268a6
Stepan Ivanov <stepan.ivanov.dev@gmail.com>
2024-05-25 19:18:13 +0700

    feat: Add endpoint to create user

--- src/users/routers.py
+++ src/users/routers.py
@@ -1,12 +1,28 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
 
 from src.database import get_db
 from . import services
 from .pagination import Pagination
-from .schemas import UserResponse
+from .schemas import UserResponse, UserCreate
 
 router = APIRouter(prefix='/api/v1/users', tags=['users'])
+
+
+@router.post(
+    '/',
+    response_model=UserResponse,
+    status_code=status.HTTP_201_CREATED
+)
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    db_user = services.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail='Email already registered'
+        )
+    user = services.create_user(db=db, user=user)
+    return user
 
 
 @router.get(
--- src/users/services.py
+++ src/users/services.py
@@ -1,7 +1,25 @@
 from sqlalchemy.orm import Session
 
 from .models import User
+from .schemas import UserCreate
+from .utils import hash_password
+
+
+def create_user(db: Session, user: UserCreate) -> User:
+    hashed_password = hash_password(user.password)
+    del user.password_confirm
+    del user.password
+    user.email = user.email.lower()
+    new_user = User(**user.model_dump(), hashed_password=hashed_password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
 
 
 def get_users(db: Session, skip: int, limit: int) -> list[User]:
     return db.query(User).offset(skip).limit(limit).all()
+
+
+def get_user_by_email(db: Session, email: str) -> User | None:
+    return db.query(User).filter(User.email == email.lower()).first()

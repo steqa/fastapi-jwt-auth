800e9040789a16c46992b0a1a54abce3f4d312be
Stepan Ivanov <stepan.ivanov.dev@gmail.com>
2024-05-25 16:49:26 +0700

    feat: Add User model

---
+++ src/users/models.py
@@ -0,0 +1,20 @@
+import datetime
+import uuid
+
+from sqlalchemy import Boolean, Column, DateTime, String
+from sqlalchemy.dialects.postgresql import UUID, BYTEA
+
+from src.database import Base
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    email = Column(String, unique=True)
+    hashed_password = Column(BYTEA)
+    is_active = Column(Boolean, default=False)
+    first_name = Column(String)
+    last_name = Column(String)
+    created_at = Column(DateTime, default=datetime.datetime.now)
+    updated_at = Column(DateTime, default=datetime.datetime.now, onupdate=datetime.datetime.now)

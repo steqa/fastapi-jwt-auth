e3a2a2d586355e1e0d42a39b21f8baefd218259c
Stepan Ivanov <stepan.ivanov.dev@gmail.com>
2024-05-28 12:46:22 +0700

    feat: Add email confirmation exceptions

---
+++ src/email_send/exceptions.py
@@ -0,0 +1,18 @@
+from fastapi import status
+from src.exceptions import BaseValidationError, BaseHTTPException
+
+
+class EmailConfirmCodeInvalid(BaseValidationError):
+    status_code = status.HTTP_422_UNPROCESSABLE_ENTITY
+    field = 'code'
+    message = 'Code invalid'
+
+
+class EmailConfirmCodeExists(BaseHTTPException):
+    status_code = status.HTTP_409_CONFLICT
+    detail = 'Code already exists for this user'
+
+
+class UserAlreadyActivated(BaseHTTPException):
+    status_code = status.HTTP_409_CONFLICT
+    detail = 'User already activated'

df9950b04dfd92ddd827b8a78c51ddd9d99bb720
Stepan Ivanov <stepan.ivanov.dev@gmail.com>
2024-05-28 10:43:46 +0700

    feat: Add password validation utility

--- src/users/utils.py
+++ src/users/utils.py
@@ -4,3 +4,7 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(password.encode(), salt)
+
+
+def validate_password(password: str, hashed_password: bytes) -> bool:
+    return bcrypt.checkpw(password.encode(), hashed_password)

44fa39fb9959c9506f8de407c94a7c3502dfd10a
Stepan Ivanov <stepan.ivanov.dev@gmail.com>
2024-05-28 13:42:32 +0700

    feat: Update local time handling in main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import locale
+
 from fastapi import FastAPI, Request, status
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
@@ -5,11 +7,15 @@ from fastapi.responses import JSONResponse
 from .email_send.routers import router as email_send_router
 from .jwt_auth.routers import router as jwt_auth_router
 from .users.routers import router as users_router
+from .config import settings
+
 app = FastAPI()
 
 app.include_router(users_router)
 app.include_router(jwt_auth_router)
 app.include_router(email_send_router)
+
+locale.setlocale(locale.LC_TIME, settings.TIME_LOCALE)
 
 
 @app.exception_handler(RequestValidationError)
